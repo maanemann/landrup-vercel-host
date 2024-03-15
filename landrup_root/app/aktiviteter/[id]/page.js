3b894c34fd7f1d2f23842524735d700a8a8be12c
maanemann <hans@niila.io>
2024-03-15 11:15:27 +0100

    Add first dynamic route data & error handling

--- landrup_root/app/aktiviteter/[id]/page.js
+++ landrup_root/app/aktiviteter/[id]/page.js
@@ -1,12 +1,44 @@
 
-// import { useApiContext } from "@/context";
+'use client'
+
+import { useApiContext } from "@/context";
+import Image from "next/image";
+import { useEffect, useState } from "react";
 
 const Aktivitetsdetaljer = ({ params }) => {
-  // const { aktiviteterData, error } = useApiContext();
+  const { aktiviteterData, error } = useApiContext();
+  const [denneAktivitet, setDenneAktivitet] = useState(null);
 
-  return ( <>
-    <h1>{ params.id }</h1>
+  useEffect(() => {
+    if (aktiviteterData) {
+      // med ´find´ metoden findes aktiviteten med `id` svarende til `prams.id`, og `params.id` konverteres fra string til tal med `Number()` (frem for `==` (loose equality), som er mindre præcist) :
+      const aktivitet = aktiviteterData.find(aktivitet => aktivitet.id === Number(params.id));
+      setDenneAktivitet(aktivitet);
+    }
+  // Der lyttes efter data fra `aktiviteterData` i dependency array'et :
+  } , [aktiviteterData]);
+
+  return denneAktivitet ? (
+    <>
+      <div className="relative w-full h-[489px]">
+        <Image
+          src={denneAktivitet.asset.url} alt={ denneAktivitet.name }
+          fill priority className="
+            object-cover
+            bg-fuchsia-950
+        "/>
+      </div>
+      <h1>{ params.id }</h1>
+    </>
+  ) : ( <>
+    { error
+      ? <h1 className="text-xl mx-4 my-12 text-red-500">{error}</h1>
+      : <h1 className="text-xl mx-4 my-12 text-themeBgBright">
+        Still loading...
+      </h1>
+    }
   </> );
 }
- 
+
 export default Aktivitetsdetaljer;
+
--- landrup_root/context.js
+++ landrup_root/context.js
@@ -6,7 +6,8 @@ import { createContext, useContext, useState, useEffect } from "react";
 const apiContext = createContext();
 
 export function ApiWrapper({ children }) {
-  const [aktiviteterData, setAktiviteterData] = useState(null);
+  // Det er vigtigt at initialisere state med en tom array, da der bruges `find` metode i `Aktivitetsdetaljer`, hvilket er en array metode, som forventer en array :
+  const [aktiviteterData, setAktiviteterData] = useState([]);
   const [error, setError] = useState(null);
 
   useEffect(() => {
@@ -30,6 +31,7 @@ export function ApiWrapper({ children }) {
     };
 
     fetchData();
+    // Dependency array sikrer at `useEffect` kun kører ved mount / unmount, ikke ved re-rendering, hvilket sker ved state update (effekten kører og opdaterer states > re-render > effekten kører ... > INFINITE LOOP). Der bruges ingen eksterne variabler i effekten, derfor tomt depency array (hvis der gjorde, skulle den køre når de ændres)
   }, []);
 
   return(
