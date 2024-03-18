ea6b0d4891344204070032881b5570f406ed4d86
maanemann <hans@niila.io>
2024-03-18 12:43:17 +0100

    Add complete search page w all bells and whistles

--- landrup_root/app/soegeside/page.js
+++ landrup_root/app/soegeside/page.js
@@ -3,47 +3,73 @@
 
 import Drawer from "@/components/Drawer";
 import { useApiContext } from "@/context";
-import Link from "next/link";
 import { useState } from "react";
+import { H1A } from "@/components/Headings";
+import { FiSearch } from "react-icons/fi";
+import AktiviteterComp from "@/components/AktiviteterComp";
 
 const Soegeside = () => {
-  const [searchTerm, setSearchTerm] = useState("");
+  const [ searchTerm, setSearchTerm ] = useState("");
   const { aktiviteterData, error } = useApiContext();
 
   const searchResults = searchTerm !== ""
     ? aktiviteterData.filter((aktivitet) => {
-      // filteret gøres case-insensitive ved at konvertere alle strings til lowercase :
+      // (filteret gøres case-insensitive ved at konvertere alle strings til lowercase) :
       return aktivitet.name.toLowerCase().includes(searchTerm.toLowerCase())
       || aktivitet.description.toLowerCase().includes(searchTerm.toLowerCase())
     })
-    // Hvis søgefeltet er tomt vises ingenting (tom array) :
     : [];
+
+    console.log(searchResults);
 
   return ( <>
 
     <Drawer />
 
     <main className="m-8">
-      {/* når value ændres, reflekteres det i searchTerm (state ↑) : */}
-      <input
-        // searchTerm som value gør dette til et "controlled component" (form element in react hvis value kontrolleres af state), og sikrer at input er 'i sync' med searchTerm (men det virker også uden) :
-        type="text" value={searchTerm}
-        // dette "controlled component" er i sync med searchTerm med `onChange` :
-        onChange={(e) => setSearchTerm(e.target.value)}
-      ></input>
 
-      {/* Søgeresultater.. (index er nødvendig for at react har en id at gå efter til hver item) : */}
-      <ul className="mt-8">
-        { searchResults.map((aktivitet, index) => (
-          <li key={index} className="mb-4">
-            <Link href={`/aktiviteter/${aktivitet.id}`} />
-            <h2 className="text-2xl">
-              { aktivitet.name }
-            </h2>
-            <p>{ aktivitet.description }</p>
-          </li>
-        )) }
-      </ul>
+      <H1A>Søg</H1A>
+
+      { error && <p className="text-red-500">{error}</p> }
+
+      {/*når value ændres, reflekteres det i searchTerm (state ↑) : */}
+      <div className="relative">
+        <input
+          // searchTerm som value gør dette til et "controlled component" (form element in react hvis value kontrolleres af state), og sikrer at input er 'i sync' med searchTerm (men det virker også uden) :
+          type="text" value={searchTerm}
+          // dette "controlled component" er i sync med searchTerm med `onChange` :
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="
+            block -mt-5 border-none outline-none
+            w-full leading-[3rem] bg-neutral-300/30 text-xl
+            text-themeBright px-4
+          "
+        ></input>
+
+        {/* ↓ Tjek den her CSS ↓ */}
+        <FiSearch className="
+          absolute top-1/2 -translate-y-1/2 right-4
+          block text-2xl
+          text-themeBright
+          pointer-events-none
+        " />
+      </div>
+
+      <div className="mt-14">
+        <AktiviteterComp filtreredeAktiviteter={searchResults} />
+      </div>
+
+      { searchTerm === "" && (
+        <p className="text-center text-2xl">
+          Søg efter en aktivitet i søgefeltet ↑
+        </p>
+      )}
+
+      { searchResults.length === 0 && searchTerm !== "" && (
+        <p className="text-center text-2xl">
+          Der blev ikke fundet nogle aktiviteter. Prøv at søge efter noget andet.
+        </p>
+      )}
     </main>
   </> );
 }
