ea68e583aba8fbe001f41363060883d95fa91739
maanemann <hans@niila.io>
2024-03-19 15:18:17 +0100

    Add message for empty calender

--- landrup_root/app/kalender/page.js
+++ landrup_root/app/kalender/page.js
@@ -4,6 +4,7 @@
 import Drawer from "@/components/Drawer";
 import { useApiContext } from "@/context";
 import { H1A } from "@/components/Headings";
+import Link from "next/link";
 
 const Kalender = () => {
   const { aktiviteterData, error } = useApiContext();
@@ -13,9 +14,20 @@ const Kalender = () => {
     <Drawer />
 
     <main className="p-8">
-      <H1A>Aktiviteter</H1A>
+      <H1A>Kalender</H1A>
 
       { error && <p className="text-red-500">{error}</p> }
+
+      <div className="text-center text-2xl mt-24 grid gap-4">
+        <p>
+          Du er ikke tilmeldt nogen aktiviteter.
+        </p>
+        <p>
+        <Link href="/aktiviteter" className="border-b-2 pb-1">
+          Gå til Aktiviteter
+        </Link>
+        </p>
+      </div>
     </main>
   </>);
 }
